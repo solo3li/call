dc409fd918b971ae709c45dfb83d5deb7fd1283f
solo3li <hamed3alii.3@gmail.com>
2026-07-03 16:59:48 +0000

    feat: add custom Dockerfile and update run script to build and deploy local Asterisk image

--- run_asterisk.sh
+++ run_asterisk.sh
@@ -4,12 +4,15 @@ set -e
 echo "Removing old Issabel container if exists..."
 docker rm -f issabel || true
 
+echo "Building custom Asterisk..."
+docker build -t my-asterisk /root/call/asterisk
+
 echo "Starting Asterisk..."
 docker run -d --name asterisk \
   --net=host \
   --restart unless-stopped \
   -v /root/call/asterisk/sip.conf:/etc/asterisk/sip.conf \
   -v /root/call/asterisk/extensions.conf:/etc/asterisk/extensions.conf \
-  andrius/asterisk:latest
+  my-asterisk
 
 echo "Asterisk is running on port 5060!"
